04ba922f1f33f9fa1c807bcbe7b41f065753f242
Bart Vaes <vaesbart80@gmail.com>
2018-06-20 16:02:02 +0200

    JmonkeyEngine samples

--- src/main/java/be/vdab/monkey/MonkeyGame.java
+++ src/main/java/be/vdab/monkey/MonkeyGame.java
@@ -5,13 +5,32 @@
  */
 package be.vdab.monkey;
 
+import com.jme3.app.SimpleApplication;
+import com.jme3.material.Material;
+import com.jme3.math.ColorRGBA;
+import com.jme3.scene.Geometry;
+import com.jme3.scene.shape.Box;
+
 /**
  *
  * @author vdabcurist
  */
-public class MonkeyGame{
+public class MonkeyGame extends SimpleApplication{
 
     public static void main(String[] args){
         System.out.println("Welcome Monkey !");
+        MonkeyGame game = new MonkeyGame();
+        game.start();
+    }
+
+    @Override
+    public void simpleInitApp(){
+        Box b = new Box(1, 1, 1); // create cube shape
+        Geometry geom = new Geometry("Box", b);  // create cube geometry from the shape
+        Material mat = new Material(assetManager,
+                "Common/MatDefs/Misc/Unshaded.j3md");  // create a simple material
+        mat.setColor("Color", ColorRGBA.Blue);   // set color of material to blue
+        geom.setMaterial(mat);                   // set the cube's material
+        rootNode.attachChild(geom);
     }
 }
